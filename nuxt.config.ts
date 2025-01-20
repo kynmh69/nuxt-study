a7c62ba89f26b2f951e72e0594e01394389bedf8
Hiroki Kayanuma <account@mail.kynmh69.net>
2025-01-20 21:51:18 +0900

    mod error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,23 @@
 import Aura from '@primevue/themes/aura';
 
 export default defineNuxtConfig({
-  modules: [
-    '@primevue/nuxt-module'
-  ],
-  primevue: {
-    /* Configuration */
+    modules: [
+        '@primevue/nuxt-module'
+    ],
     primevue: {
-      options: {
-        theme: {
-          preset: Aura
+        options: {
+            theme: {
+                preset: Aura
+            }
         }
-      }
-    }
-  },
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
     },
-  },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
