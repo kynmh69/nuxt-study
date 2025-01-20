12230f32b3ba01adb4d5c7e97266fe3ab952084f
Hiroki Kayanuma <account@mail.kynmh69.net>
2025-01-20 22:35:00 +0900

    mod primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,5 +20,10 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
-    devtools: {enabled: true},
+    devtools: {
+        enabled: true,
+        timeline: {
+            enabled: true,
+        }
+    },
 })
