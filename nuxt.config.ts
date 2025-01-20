d64d098fdfe0961e8a1fe49628f9b724624df874
hiroki.kayanuma <hiroki.kayanuma.od@ambl.co.jp>
2025-01-20 14:55:36 +0900

    add primevue framework.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    /* Configuration */
+    primevue: {
+      options: {
+        theme: {
+          preset: Aura
+        }
+      }
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
